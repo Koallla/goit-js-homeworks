5aab9d26d3bc6929cc6e0778c3892a99b1dfe62d
zmiyov <zmiyov@i.ua>
2019-08-06 16:20:34 +0300

    fix task3/4

---
+++ goit-js-hw-03/js/task_3.js
@@ -0,0 +1,40 @@
+'use strict';
+
+function findBestEmployee(employees) {
+  let name;
+  let maxQuantityTasks = 0;
+
+  for (const key in employees) {
+    if (maxQuantityTasks < employees[key]) {
+      maxQuantityTasks = employees[key];
+      name = key;
+    }
+  }
+  return name;
+}
+
+console.log(
+  findBestEmployee({
+    ann: 29,
+    david: 35,
+    helen: 1,
+    lorence: 99,
+  }),
+); // lorence
+
+console.log(
+  findBestEmployee({
+    poly: 12,
+    mango: 17,
+    ajax: 4,
+  }),
+); // mango
+
+console.log(
+  findBestEmployee({
+    lux: 147,
+    david: 21,
+    kiwi: 19,
+    chelsy: 38,
+  }),
+); // lux
