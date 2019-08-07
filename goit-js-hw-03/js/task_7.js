43251ec39a91db0a63318d29faba91c3eb97728a
Koallla <zmiyov2422@gmail.com>
2019-08-07 18:23:50 +0300

    added task7

--- goit-js-hw-03/js/task_7.js
+++ goit-js-hw-03/js/task_7.js
@@ -16,9 +16,13 @@ const account = {
    * Создает объект транзакции и вызывает addTransaction
    */
   deposit(amount) {
-    let transactions = {
+    let id = this.transactions.length;
+    const transaction = {
+      id: id,
+      type: 'deposit',
       amount,
     };
+    this.addTransaction(transaction);
 
     this.balance += amount;
   },
@@ -30,29 +34,77 @@ const account = {
    * Если amount больше чем текущий баланс, выводи сообщение
    * о том, что снятие такой суммы не возможно, недостаточно средств.
    */
-  withdraw(amount) {},
+  withdraw(amount) {
+    let id = this.transactions.length;
+    const transaction = {
+      id: id,
+      type: 'withdraw',
+      amount,
+    };
+
+    if (amount > this.balance) {
+      alert('Cнятие такой суммы не возможно, недостаточно средств');
+      return;
+    } else {
+      this.addTransaction(transaction);
+
+      this.balance -= amount;
+    }
+  },
 
   /*
-   * Метод добавляющий транзацию в свойство transactions
+   * Метод добавляющий транзакцию в свойство transactions
    * Принимает объект транзакции
    */
-  addTransaction(transaction) {},
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+    console.log(this.transactions);
+  },
 
   /*
    * Метод возвращает текущий баланс
    */
-  getBalance() {},
+  getBalance() {
+    console.log(this.balance);
+    return;
+  },
 
   /*
    * Метод ищет и возвращает объект транзации по id
    */
-  getTransactionDetails(id) {},
+  getTransactionDetails(id) {
+    for (let i = 0; i < this.transactions.length; i += 1) {
+      const transactionsObject = this.transactions[i];
+
+      if (transactionsObject.id === id) {
+        console.log(transactionsObject);
+        return;
+      }
+    }
+  },
 
   /*
    * Метод возвращает количество средств
    * определенного типа транзакции из всей истории транзакций
    */
-  getTransactionTotal(type) {},
+  getTransactionTotal(type) {
+    let total = 0;
+    for (let i = 0; i < this.transactions.length; i += 1) {
+      const transactionsObject = this.transactions[i];
+      if (transactionsObject.type === type) {
+        total += transactionsObject.amount;
+      }
+    }
+    console.log(total);
+    return;
+  },
 };
 
-console.log(account.deposit(100));
+account.deposit(100);
+account.deposit(200);
+account.withdraw(150);
+account.getBalance();
+account.getTransactionDetails(2);
+account.getTransactionTotal('deposit');
+account.getBalance();
+account.withdraw(300);
