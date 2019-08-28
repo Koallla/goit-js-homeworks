1d18ab45f9648bd1ce355a85ea51f5b73fa595ac
zmiyov <zmiyov@i.ua>
2019-08-28 11:10:27 +0300

    fixed tasks 3, 9

--- goit-js-hw-06/js/task_1.js
+++ goit-js-hw-06/js/task_1.js
@@ -10,17 +10,12 @@ const getUsersWithEyeColor = (users, color) =>
   users.filter(user => user.eyeColor === color);
 console.log(getUsersWithEyeColor(users, 'blue'));
 
-//Task 3
+// Task 3
 const getUsersWithGender = (users, gender) =>
-  users.reduce((nameByGender, user) => {
-    if (user.gender === gender) {
-      nameByGender.push(user.name);
-    }
-    return nameByGender;
-  }, []);
+  users.filter(user => user.gender === gender).map(user => user.name);
 console.log(getUsersWithGender(users, 'male'));
 
-//Task 4
+// Task 4
 const getInactiveUsers = users => users.filter(user => !user.isActive);
 console.log(getInactiveUsers(users));
 
@@ -29,11 +24,13 @@ const getUserWithEmail = (users, email) =>
   users.find(user => user.email === email);
 console.log(getUserWithEmail(users, 'shereeanthony@kog.com'));
 console.log(getUserWithEmail(users, 'elmahead@omatom.com'));
+
 //Task 6
 const getUsersWithAge = (users, min, max) =>
   users.filter(user => min < user.age && user.age < max);
 console.log(getUsersWithAge(users, 20, 30));
 console.log(getUsersWithAge(users, 30, 40));
+
 //Task 7
 const calculateTotalBalance = users =>
   users.reduce(
@@ -41,24 +38,23 @@ const calculateTotalBalance = users =>
     0,
   );
 console.log(calculateTotalBalance(users));
+
 //Task 8
-const getUsersWithFriend = (users, friendName) => {
-  return users.reduce((allNamesFriens, user) => {
-    if (user.friends.includes(friendName)) allNamesFriens.push(user.name);
-    return allNamesFriens;
-  }, []);
-};
+const getUsersWithFriend = (users, friendName) =>
+  users
+    .filter(user => user.friends.includes(friendName))
+    .map(user => user.name);
 console.log(getUsersWithFriend(users, 'Briana Decker'));
 console.log(getUsersWithFriend(users, 'Goldie Gentry'));
+
 //Task 9
-const getNamesSortedByFriendsCount = users => {
-  const usersSort = [...users]; //Иммутация
-  usersSort
+const getNamesSortedByFriendsCount = users =>
+  users
     .sort((a, b) => a.friends.length - b.friends.length)
     .map(user => user.name);
-  return usersSort;
-};
+
 console.log(getNamesSortedByFriendsCount(users));
+
 //Task 10
 
 const getAllskills = users =>
